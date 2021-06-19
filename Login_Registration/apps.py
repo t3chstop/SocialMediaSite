7e2e01567130dca75528282df82ad9fee29b9ca6
Siddarth <randomman3578@gmail.com>
2021-06-19 15:10:33 -0700

    First official commit

---
+++ Login_Registration/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class LoginRegistrationConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Login_Registration'
---
+++ Login_Registration/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from .forms import SignUpForm
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def sign_up(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return render(request, 'dashboard.html')
+    else:
+        form = SignUpForm()
+    return render(request, 'sign_up.html', {'form': form})
