5e3bfd63498a56a2223d206cfc1646c5f66ef9df
t3chstop <84115793+t3chstop@users.noreply.github.com>
2021-08-21 19:58:23 -0700

    display friends list in dashboard

--- Friends/views.py
+++ Friends/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
-from friendship.models import Friend, Follow, Block # type: ignore
-# Create your views here.
+from django.shortcuts import render, redirect
+from friendship.models import Friend, FriendshipRequest # type: ignore
 
+# Create your views here.
