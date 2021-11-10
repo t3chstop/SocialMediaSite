26099f573c7cea92a5f37cf770553c563db07f59
t3chstop <84115793+t3chstop@users.noreply.github.com>
2021-11-09 21:36:19 -0800

    Added username with message payload

--- chat/views.py
+++ chat/views.py
@@ -1,10 +1,16 @@
+from django.contrib.auth import login
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
 def index(request):
     return render(request, 'chat/index.html')
 
+@login_required
 def room(request, room_name):
+    displayName = request.user.display_name
     return render(request, 'chat/room.html', {
-        'room_name': room_name
+        'room_name': room_name,
+        'displayName' : displayName
     })
