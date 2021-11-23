284b9b9bba1c89b3b70282cdab05d32a019b98f5
t3chstop <84115793+t3chstop@users.noreply.github.com>
2021-11-22 20:37:24 -0800

    Initiate rewrite

--- Friends/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render, redirect
-from friendship.models import Friend, FriendshipRequest # type: ignore
-
-# Create your views here.
--- SocialMediaSite/wsgi.py
+++ SocialMediaSite/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Social_Media_site project.
+WSGI config for SocialMediaSite project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Social_Media_site.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SocialMediaSite.settings')
 
 application = get_wsgi_application()
