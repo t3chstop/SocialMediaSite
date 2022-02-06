4b6f312b8f2faf86791463390783a70b6a46f035
t3chstop <84115793+t3chstop@users.noreply.github.com>
2022-02-05 21:15:44 -0800

    Add models for chatrom and message

--- SocialMediaSite/urls.py
+++ SocialMediaSite/urls.py
@@ -13,6 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+"""
+Planned URL Scheme:
+domain.com - Show a welcome page that sells the site
+domain.com/register/ - Sign up/registration page. Users create accounts here
+domain.com/login/ - Login page.
+domain.com/dashboard/ - Dashboard page
+domain.com/profile/<user>/ - User profile
+domain.com/edit-profile/ - Edit user profile
+domain.com/logout/ - Logout(will contain template. just a redirect)
+domain.com/chat/ - Page with all of users chatrooms
+domain.com/chat/ws/key - Page with an active chatroom(ws indicates websocket connection)
+"""
+
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,25 @@
+from operator import mod
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class ChatRoom(models.Model):
+    name = models.CharField(max_length=100, unique=False, blank=False)
+    user = models.ManyToManyField()
+    key = models.BigAutoField(primary_key=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Message(models.Model):
+    room = models.ForeignKey(ChatRoom, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    content = models.TextField(max_length=1000, unique=False, blank=True)
+
+    class Meta:
+        ordering = ('timestamp',)
+
+    def __str__(self):
+        return self.content
